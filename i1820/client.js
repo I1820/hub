8522700ecb56ced432bf22420a7eda063a1fe3a4
Parham Alvani <parham.alvani@gmail.com>
2017-07-10 14:49:55 +0430

    I1820 client was improved.

--- i1820/client.js
+++ i1820/client.js
@@ -7,28 +7,41 @@
  * | File Name:     client.js
  * +===============================================
  */
-const mqtt = require('mqtt');
+const mqtt = require('mqtt')
+const EventEmitter = require('events')
 
-const I1820Thing = require('./thing');
+const I1820Thing = require('./thing')
 
-class I1820Client {
-  constructor(url, tenantId) {
-    this.client  = mqtt.connect(url);
-    this.tenantId = tenantId;
-    this.things = [];
-  }
+class I1820Client extends EventEmitter {
+  constructor (url, name, tenantId) {
+    super()
 
-  start() {
+    this.client = mqtt.connect(url, {
+      clientId: `I1820/${tenantId}/agent/${name}`
+    })
+
+    this.client.on('message', (topic, message, packet) => {
+      if (topic === `I1820/${tenantId}/agent/${name}`) {
+        this._id = message
+        this.emit('ready')
+      } else if (topic === `I1820/${tenantId}/configuration`) {
+      }
+    })
+
+    this.tenantId = tenantId
+    this.name = name
+    this.things = []
+
     setInterval(() => {
-      this.client.publish(`I1820/${this.tenantId}/agent/ping`,);
-    }, 10);
+      this.client.publish(`I1820/${this.tenantId}/agent/ping`)
+    }, 10)
   }
 
-  addThing(id, type) {
-    const t = new I1820Thing(this.client, id, null, type);
-    this.things.push(t);
-    return t;
+  addThing (id, type) {
+    const t = new I1820Thing(this.client, id, null, type)
+    this.things.push(t)
+    return t
   }
 }
 
-module.exports = I1820Client;
+module.exports = I1820Client
