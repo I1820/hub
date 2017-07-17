8b83b6682423d2ef0e0124e8e3c03fad651ec4d1
Parham Alvani <parham.alvani@gmail.com>
2017-07-17 07:42:36 +0430

    Renames id into hash.

--- examples/newbie.js
+++ examples/newbie.js
@@ -2,5 +2,5 @@ const i1820 = require('../')
 
 let client = new i1820.I1820Client('mqtt://127.0.0.1', 'parham_home', 'Newbie')
 client.on('ready', () => {
-  console.log(client._id)
+  console.log(client.hash)
 })
