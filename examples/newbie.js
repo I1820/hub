094afb0bdd8ea2b797f0346cb6a1c2ba8a9b106c
Parham Alvani <parham.alvani@gmail.com>
2017-07-22 08:48:07 +0430

    Improves newbie example.

--- examples/newbie.js
+++ examples/newbie.js
@@ -4,3 +4,10 @@ let client = new i1820.I1820Client('mqtt://127.0.0.1', 'parham_home', 'Newbie')
 client.on('ready', () => {
   console.log(client.hash)
 })
+const t1 = client.addThing('7:1', 'temperature')
+setInterval(() => {
+  t1.log({
+    temperature: 10
+  })
+  console.log('log temperature')
+}, 1000)
