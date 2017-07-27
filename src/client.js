094288efec653ca313fd29781ac566bdbff589a5
Parham Alvani <parham.alvani@gmail.com>
2017-07-28 01:18:50 +0430

    Completes bamboo client.

--- src/client.js
+++ src/client.js
@@ -11,14 +11,14 @@ const mqtt = require('mqtt')
 const Loki = require('lokijs')
 const EventEmitter = require('events')
 
-const I1820Thing = require('./thing')
+const BambooThing = require('./thing')
 const Message = require('./message')
 
-class I1820Client extends EventEmitter {
+class BambooClient extends EventEmitter {
   constructor (url, tenant, name) {
     super()
 
-    this.db = new Loki('I1820')
+    this.db = new Loki('Bamboo')
     this.logs = this.db.addCollection('logs')
     this.tenant = tenant
     this.hash = null
@@ -26,17 +26,17 @@ class I1820Client extends EventEmitter {
     this.things = []
 
     this.client = mqtt.connect(url, {
-      clientId: `I1820/${tenant}/agent/${name}`
+      clientId: `Bamboo/${tenant}/agent/${name}`
     })
 
     this.client.subscribe([
-      `I1820/${tenant}/agent/${name}`,
-      `I1820/${tenant}/agent/configuration`
+      `Bamboo/${tenant}/agent/${name}`,
+      `Bamboo/${tenant}/agent/configuration`
     ])
 
     this.client.on('message', (topic, message, packet) => {
       console.log(topic)
-      if (topic === `I1820/${tenant}/agent/${name}` && !this.hash) {
+      if (topic === `Bamboo/${tenant}/agent/${name}` && !this.hash) {
         this.hash = message.toString()
         this.emit('ready')
         this._start()
@@ -47,14 +47,14 @@ class I1820Client extends EventEmitter {
   _start () {
     setInterval(() => {
       /* ping */
-      this.client.publish(`I1820/${this.tenant}/agent/ping`,
+      this.client.publish(`Bamboo/${this.tenant}/agent/ping`,
         JSON.stringify(new Message(this.hash, this.name, '')))
     }, 10000)
     setInterval(() => {
       /* log */
       this.logs.where((log) => {
         console.log(log)
-        this.client.publish(`I1820/${this.tenant}/agent/log`,
+        this.client.publish(`Bamboo/${this.tenant}/agent/log`,
           JSON.stringify(new Message(this.hash, this.name, log)))
         this.logs.remove(log)
       })
@@ -62,10 +62,10 @@ class I1820Client extends EventEmitter {
   }
 
   addThing (id, type) {
-    const t = new I1820Thing(id, type, this.logs)
+    const t = new BambooThing(id, type, this.logs)
     this.things.push(t)
     return t
   }
 }
 
-module.exports = I1820Client
+module.exports = BambooClient
