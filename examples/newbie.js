8d875118b2b47800a5d0f9bfebf247feaeed13f4
Parham Alvani <parham.alvani@gmail.com>
2017-08-31 16:50:36 +0430

    Adds remote logging into newbie example.

--- examples/newbie.js
+++ examples/newbie.js
@@ -1,5 +1,7 @@
 const bamboo = require('../')
 
+let logger = new bamboo.BambooLogger('parham_home', 'Newbie', '127.0.0.1', 9003)
+
 let client = new bamboo.BambooClient('mqtt://127.0.0.1', 'parham_home', 'Newbie')
 client.on('ready', () => {
   console.log(client.hash)
@@ -9,5 +11,6 @@ setInterval(() => {
   t1.log({
     temperature: 10
   })
+  logger.log('info', 'log temperature')
   console.log('log temperature')
 }, 10000)
